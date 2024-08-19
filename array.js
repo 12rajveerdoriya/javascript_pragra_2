efea4b4207c5b4ca9a9d7cc0ed7daeaaae2e2576
rajveer doriya <rajveerdoriyaofficial@gmail.com>
2024-08-19 14:07:10 +0530

    array methods

--- array.js
+++ array.js
@@ -1,1 +1,46 @@
 //array 
+
+const  myarray=[0,1,2,3,4,5,6];
+// console.log(myarray);
+// console.log(myarray[4]-1);
+
+console.log(myarray.length);
+
+/*
+//add  value in array 
+console.log(myarray.push(7));
+console.log(myarray);
+
+//+++
+console.log(myarray.unshift(9));
+
+//----
+//delete value 
+console.log(myarray.pop());
+console.log(myarray);
+
+//++
+*/
+/*
+console.log(myarray.shift());
+console.log(myarray);
+
+
+console.log(myarray.includes(9));
+
+console.log(myarray.indexOf(6));
+
+*/
+// console.log(typeof (myarray.join()));
+
+
+//slice ,splice 
+
+console.log("A",myarray);
+console.log(myarray.slice(2,2));
+
+
+console.log(myarray.splice(2,4))
+console.log("B",myarray);
+
+
